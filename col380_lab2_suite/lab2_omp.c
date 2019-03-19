6b3676331bf3915866981add6cb9d7739c79ee2b
Chetan Mittal <mittalchetan98@gmail.com>
2019-03-19 16:47:49 +0530

    submission

--- col380_lab2_suite/lab2_omp.c
+++ col380_lab2_suite/lab2_omp.c
@@ -7,14 +7,14 @@
 //./pca testcase/testcase_2_2 90
 
 // sort in decreasing order
-void revSort(int N, float* W, float* W_n){
+void revSort(int N, double* W, double* W_n){
 	int i, j; 
 	for(i = 0; i < N; i++)
 		W_n[i] = W[i];
 	for (i = 0; i < N-1; i++){    
 		for (j = 0; j < N-i-1; j++){  
 	    	if (W_n[j] < W_n[j+1]){
-	    		float temp = W_n[j];
+	    		double temp = W_n[j];
 	    		W_n[j] = W_n[j+1];
 	    		W_n[j+1] = temp;
 	    	}
@@ -23,23 +23,23 @@ void revSort(int N, float* W, float* W_n){
 }
 
 //get new M*N matrix;
-float** newMatrix(int M, int N){
-	float** W;
-	W = (float **)malloc(M*sizeof(float*));
+double** newMatrix(int M, int N){
+	double** W;
+	W = (double **)malloc(M*sizeof(double*));
 #pragma omp parallel for //num_threads(4)
 	for(int i = 0; i < M; i++)
-	    W[i]= (float*)malloc(N*sizeof(float));
+	    W[i]= (double*)malloc(N*sizeof(double));
 	return W;
 }
 
-void freeMatrix(int M, int N, float** W){
+void freeMatrix(int M, int N, double** W){
 	for(int i = 0; i < M; i++)
 	    free(W[i]);
 	free(W);
 }
 
 //initialise N*N matrix as identity
-void identity(int N, float** W){
+void identity(int N, double** W){
 #pragma omp parallel for //num_threads(4)
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < N; j++){
@@ -51,7 +51,7 @@ void identity(int N, float** W){
 }
 
 //initialise N*N matrix as identity
-void identityLinear(int N, float* W){
+void identityLinear(int N, double* W){
 #pragma omp parallel for //num_threads(4)
 	for(int i = 0; i < N; i++){
 		for(int j = 0; j < N; j++){
@@ -63,7 +63,7 @@ void identityLinear(int N, float* W){
 }
 
 //initialise M*N matrix as zero
-void zeros(int M, int N, float** W){
+void zeros(int M, int N, double** W){
 #pragma omp parallel for //num_threads(4)
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < N; j++){
@@ -72,7 +72,7 @@ void zeros(int M, int N, float** W){
 	}
 }
 
-void zerosLinear(int M, int N, float* W){
+void zerosLinear(int M, int N, double* W){
 #pragma omp parallel for //num_threads(4)
 	for(int i = 0; i < M*N; i++){
 		W[i] = 0.0;
@@ -80,7 +80,7 @@ void zerosLinear(int M, int N, float* W){
 }
 
 // copy M*N matrix
-void copyM(int M, int N, float** W, float** W_n){
+void copyM(int M, int N, double** W, double** W_n){
 #pragma omp parallel for //num_threads(4)
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < N; j++){
@@ -89,7 +89,7 @@ void copyM(int M, int N, float** W, float** W_n){
 	}
 }
 
-void copyMToLinear(int M, int N, float** W, float* W_n){
+void copyMToLinear(int M, int N, double** W, double* W_n){
 #pragma omp parallel for //num_threads(4)
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < N; j++){
@@ -98,7 +98,7 @@ void copyMToLinear(int M, int N, float** W, float* W_n){
 	}
 }
 
-void copyLinearToM(int M, int N, float* W, float** W_n){
+void copyLinearToM(int M, int N, double* W, double** W_n){
 #pragma omp parallel for //num_threads(4)
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < N; j++){
@@ -107,7 +107,7 @@ void copyLinearToM(int M, int N, float* W, float** W_n){
 	}
 }
 
-void copyLinearToLinear(int M, int N, float* W, float* W_n){
+void copyLinearToLinear(int M, int N, double* W, double* W_n){
 #pragma omp parallel for //num_threads(4)
 	for(int i = 0; i < M*N; i++){
 		W_n[i] = W[i];
@@ -115,7 +115,7 @@ void copyLinearToLinear(int M, int N, float* W, float* W_n){
 }
 
 // print M*N matrix
-void printM(int M, int N, float** W){
+void printM(int M, int N, double** W){
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < N; j++){
 			printf("%f ", W[i][j]);
@@ -126,7 +126,7 @@ void printM(int M, int N, float** W){
 }
 
 // transpose of M*N matrix W
-void transpose(int M, int N, float** W, float** W_T){
+void transpose(int M, int N, double** W, double** W_T){
 #pragma omp parallel for //num_threads(4) schedule(static)
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < N; j++){
@@ -136,11 +136,11 @@ void transpose(int M, int N, float** W, float** W_T){
 }
 
 // multiple M*N matrix with N*R matrix
-void multiply(int M, int N, int R, float** W1, float** W2, float** W_M){
+void multiply(int M, int N, int R, double** W1, double** W2, double** W_M){
 #pragma omp parallel for num_threads(4) schedule(static)
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < R; j++){
-			float sum = 0.0;
+			double sum = 0.0;
 			for(int k = 0; k < N; k++){
 				sum += W1[i][k] * W2[k][j];
 			}
@@ -150,11 +150,11 @@ void multiply(int M, int N, int R, float** W1, float** W2, float** W_M){
 }
 
 // multiple M*N matrix with N*R matrix in 1D array
-void multiplyLinear(int M, int N, int R, float* W1, float* W2, float* W_M){
+void multiplyLinear(int M, int N, int R, double* W1, double* W2, double* W_M){
 #pragma omp parallel for //num_threads(4) schedule(static)
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < R; j++){
-			float sum = 0.0;
+			double sum = 0.0;
 			for(int k = 0; k < N; k++){
 				sum += W1[i*N + k] * W2[k*R + j];
 			}
@@ -163,7 +163,7 @@ void multiplyLinear(int M, int N, int R, float* W1, float* W2, float* W_M){
 	}
 }
 
-float nabs(float a){
+double nabs(double a){
 	if(a > 0)
 		return a;
 	return -a;
@@ -171,14 +171,14 @@ float nabs(float a){
 
 // QR factors of N*N matrix
 // returns weird answers some of te times
-void QRfactors(int N, float** W, float** Q, float** R){
+void QRfactors(int N, double** W, double** Q, double** R){
 
-	float** V = newMatrix(N, N);
+	double** V = newMatrix(N, N);
 	transpose(N, N, W, V);
 
 	for(int i = 0; i < N; i++){
 		R[i][i] = 0.0;
-		float norm = 0.0;
+		double norm = 0.0;
 		for(int k = 0; k < N; k++){
 			norm += V[i][k]*V[i][k];
 		}
@@ -198,14 +198,14 @@ void QRfactors(int N, float** W, float** Q, float** R){
 	}
 }
 
-void QRfactorsLinear(int N, float* W, float* Q, float* R){
+void QRfactorsLinear(int N, double* W, double* Q, double* R){
 
-	float* V = (float*)malloc(N*N*sizeof(float));
+	double* V = (double*)malloc(N*N*sizeof(double));
 	copyLinearToLinear(N, N, W, V);
 
 	for(int i = 0; i < N; i++){
 		R[i*N + i] = 0.0;
-		float norm = 0.0;
+		double norm = 0.0;
 		for(int k = 0; k < N; k++){
 			norm += V[k*N + i]*V[k*N + i];
 		}
@@ -233,38 +233,38 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 {
 	int i,j,k,t;
 
-	float **D2;	//M*N matrix
+	double **D2;	//M*N matrix
 	D2 = newMatrix(M, N);
 
 	for(i = 0; i < M; i++){
 		for(j = 0; j < N; j++)
-			D2[i][j] = D[i*N + j];
+			D2[i][j] = (double)D[i*N + j];
 	}
 
 	/* No need for taking transpose, replace U by V, V by U, sigma by sigma^T in the final output */
 
-	float **D1;	//M*N matrix
+	double **D1;	//M*N matrix
 	D1 = newMatrix(M, N);
 	copyM(M, N, D2, D1);
 
-	float **D_T;	//N*M matrix
+	double **D_T;	//N*M matrix
 	D_T = newMatrix(N, M);
 	
 	transpose(M, N, D1, D_T);
 
-	float **DT_D;	//N*N matrix
+	double **DT_D;	//N*N matrix
 	DT_D = newMatrix(N, N);
 
 	multiply(N, M, N, D_T, D1, DT_D);	
 
 	/********* QR Algorithm *********/
 	/********************************/
-	float **D_i = newMatrix(N, N);	//N*N matrix
-	float **D_new = newMatrix(N, N);	//N*N matrix
-	float **E_i = newMatrix(N, N);  //N*N matrix
-	float **E_new = newMatrix(N, N);  //N*N matrix
-	float **Q = newMatrix(N, N);  //N*N matrix
-	float **R = newMatrix(N, N);  //N*N matrix
+	double **D_i = newMatrix(N, N);	//N*N matrix
+	double **D_new = newMatrix(N, N);	//N*N matrix
+	double **E_i = newMatrix(N, N);  //N*N matrix
+	double **E_new = newMatrix(N, N);  //N*N matrix
+	double **Q = newMatrix(N, N);  //N*N matrix
+	double **R = newMatrix(N, N);  //N*N matrix
 	
 	copyM(N, N, DT_D, D_i);
 	identity(N, E_i);
@@ -272,7 +272,7 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 	zeros(N, N, R);
 
 	int CONG_LIMIT = 10000;
-	float TOLERANCE = 0.000000001;
+	double TOLERANCE = 0.0000000001;
 	iter_count = 0;
 
 	while(iter_count < CONG_LIMIT){
@@ -283,15 +283,9 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 		multiply(N, N, N, R, Q, D_new);
 		multiply(N, N, N, E_i, Q, E_new);
 
-		float maxDiff = 0.0;
-		float diff;
-		/*for(i = 0; i < N; i++){
-			diff = nabs(D_new[i][i] - D_i[i][i]);	
-			if(diff > maxDiff)
-				maxDiff = diff;
-			if(maxDiff > TOLERANCE)
-				break;
-		}*/
+		double maxDiff = 0.0;
+		double diff;
+
 		for(i = 0; i < N; i++){
 			for(j = 0; j < N; j++){
 				diff = E_new[i][j] - E_i[i][j];
@@ -307,7 +301,7 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 		if(maxDiff < TOLERANCE)
 			break;
 
-		float** temp;
+		double** temp;
 		temp = E_new;
 		E_new = E_i;
 		E_i = temp;
@@ -324,9 +318,9 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 
 	/**********QR Finish************/
 
-	float* eigen_values = (float*)malloc(N*sizeof(float));
-	float* sing_values = (float*)malloc(N*sizeof(float));
-	float* sing_values_sorted = (float*)malloc(N*sizeof(float));
+	double* eigen_values = (double*)malloc(N*sizeof(double));
+	double* sing_values = (double*)malloc(N*sizeof(double));
+	double* sing_values_sorted = (double*)malloc(N*sizeof(double));
 
 	for(i = 0; i < N; i++){
 		eigen_values[i] = D_i[i][i];
@@ -335,8 +329,8 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 
 	revSort(N, sing_values, sing_values_sorted);
 
-	float** sig = newMatrix(M, N);
-	float** sig_inv = newMatrix(N, M);
+	double** sig = newMatrix(M, N);
+	double** sig_inv = newMatrix(N, M);
 	zeros(M, N, sig);
 	zeros(N, M, sig_inv);
 
@@ -351,10 +345,10 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 		sig_inv[i][i] = 1/sing_values_sorted[i];
 	}
 
-	float** V = newMatrix(N, N);
+	double** V = newMatrix(N, N);
 	zeros(N, N, V);
 	for(i = 0; i < numEigenValues; i++){
-		float temp = sing_values_sorted[i];
+		double temp = sing_values_sorted[i];
 		int index = j;
 		for(j = 0; j < N; j++){
 			if(sing_values[j] == temp){
@@ -366,36 +360,36 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 			V[k][i] = E_i[k][index];
 	}
 
-	float** V_T1 = newMatrix(N, N);
+	double** V_T1 = newMatrix(N, N);
 	transpose(N, N, V, V_T1);
 
-	float** DV = newMatrix(M, N);
+	double** DV = newMatrix(M, N);
 	multiply(M, N, N, D1, V, DV);
 
-	float** U1 = newMatrix(M, M);
+	double** U1 = newMatrix(M, M);
 	multiply(M, N, M, DV, sig_inv, U1);
 
 	for(i = 0; i < N; i++){
 		for(j = 0; j < N; j++){
-			U[0][i*N + j] = V[i][j];
+			U[0][i*N + j] = (float)V[i][j];
 		}
 	}
 
 	for(i = 0; i < M; i++){
 		for(j = 0; j < M; j++){
-			V_T[0][i*M + j] = U1[j][i];
+			V_T[0][i*M + j] = (float)U1[j][i];
 		}
 	}
 
 	for(i = 0; i < N; i++){
-		SIGMA[0][i] = sig[i][i];
+		SIGMA[0][i] = (float)sig[i][i];
 	}
 
 	//printf("SVD done\n\n");
 
 }
 
-// /* *A = float(m(m*n*(flar* )))
+// /* *A = double(m(m*n*(flar* )))
 // 	*****************************************************
 // 		TODO -- You must implement this function
 // 	*****************************************************
@@ -404,33 +398,33 @@ void PCA(int retention, int M, int N, float* D, float* U, float* SIGMA, float** 
 {
 	int i,j,k;
 
-	float **D2;	//M*N matrix
+	double **D2;	//M*N matrix
 	D2 = newMatrix(M, N);
 
 	for(i = 0; i < M; i++){
 		for(j = 0; j < N; j++)
-			D2[i][j] = D[i*N + j];
+			D2[i][j] = (double)D[i*N + j];
 	}
 
-	float sum_eigenValues = 0.0;
+	double sum_eigenValues = 0.0;
 	for(i = 0; i < N; i++)
-		sum_eigenValues += (SIGMA[i]*SIGMA[i]);
+		sum_eigenValues += (double)(SIGMA[i]*SIGMA[i]);
 
-	float sumRet = 0.0;
+	double sumRet = 0.0;
 	for(i = 0; i < N; i++){
 		sumRet += (SIGMA[i]*SIGMA[i])/sum_eigenValues;
 		if(sumRet * 100 >= retention)
 			break;
 	}   
 	K[0] = i + 1;
-	printf("K = %d\n\n", K[0]);
-	float** W = newMatrix(N, K[0]);
-	float** D_HAT1 = newMatrix(M, K[0]);
+	//printf("K = %d\n\n", K[0]);
+	double** W = newMatrix(N, K[0]);
+	double** D_HAT1 = newMatrix(M, K[0]);
 	D_HAT[0] = (float*)malloc(sizeof(float)*M*K[0]);
 
 	for(i = 0; i < N; i++){
 		for(j = 0; j < K[0]; j++){
-			W[i][j] = U[i*N + j];
+			W[i][j] = (double)U[i*N + j];
 		}
 	}
 
@@ -438,13 +432,13 @@ void PCA(int retention, int M, int N, float* D, float* U, float* SIGMA, float** 
 	
 	multiply(M, N, K[0], D2, W, D_HAT1);
 
-	printM(M, K[0], D_HAT1);
+	//printM(M, K[0], D_HAT1);
 
-	printf("\n\n%d\n", iter_count);
+	//printf("\n%f\n%d\n", sumRet, iter_count);
 
 	for(i = 0; i < M; i++){
 		for(j = 0; j < K[0]; j++){
-			D_HAT[0][i*K[0] + j] = D_HAT1[i][j];
+			D_HAT[0][i*K[0] + j] = (float)D_HAT1[i][j];
 		}
 	}
 }
